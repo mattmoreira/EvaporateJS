4536c0a9e033dab4d42970d24420928a11ceeeca
Matheus Moreira <mattelias@live.com>
2020-04-27 19:11:09 +0200

    Remove instrumented code

--- codemod/output/CancelableS3AWSRequest.ts
+++
@@ -1,9 +0,0 @@
-import { SignedS3AWSRequest } from './SignedS3AWSRequest'
-import { ABORTED, CANCELED } from './Constants'
-
-class CancelableS3AWSRequest extends SignedS3AWSRequest {
-  errorExceptionStatus() {
-    return [ABORTED, CANCELED].includes(this.fileUpload.status)
-  }
-}
-export { CancelableS3AWSRequest }
