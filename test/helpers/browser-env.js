ed79322b7c9dda4bb1c978b8cf327f595901a193
Matheus Moreira <mattelias@live.com>
2020-08-30 20:06:22 +0200

    fix old reference to evaporate codemod

--- test/helpers/browser-env.js
+++ test/helpers/browser-env.js
@@ -1,7 +1,7 @@
 import { DOMParser } from 'xmldom'
 import { jsdom } from 'jsdom'
 import { File } from 'file-api'
-import Evaporate from '../../evaporate'
+import Evaporate from '../../dist/bundle'
 import sinon from 'sinon'
 import initResponse from '../fixtures/init-response'
 import completeResponse from '../fixtures/complete-response'
